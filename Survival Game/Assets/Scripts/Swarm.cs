214b92e7605b9fd71bbeccae2ffd55b90e2e4bc0
marcus-elia <mselia@uvm.edu>
2021-01-04 09:50:47 -0500

    Added basic swarm scripts

---
+++ Survival Game/Assets/Scripts/Swarm.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swarm : MonoBehaviour
+{
+    private Transform playerTransform;
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, playerTransform.position) > speed)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, speed);
+        }
+    }
+
+    public void SetPlayerTransform(Transform input)
+    {
+        playerTransform = input;
+    }
+    public void SetPosition(Vector3 input)
+    {
+        transform.position = input;
+    }
+    public void SetSpeed(float inputSpeed)
+    {
+        speed = inputSpeed;
+    }
+}
---
+++ Survival Game/Assets/Scripts/SwarmManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmManager : MonoBehaviour
+{
+    public GameObject swarmPrefab;
+
+    public Transform playerTransform;
+    public float spawnRadius;
+    public float averageSpawnTime;
+    public float spawnIntervalWidth;
+    public float speed;
+    public float spawnHeight;
+
+    private float currentSpawnWait;
+    private float lastSpawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastSpawnTime = Time.time;
+        currentSpawnWait = GetRandomSpawnWait();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - lastSpawnTime > currentSpawnWait)
+        {
+            SpawnSwarm();
+            lastSpawnTime = Time.time;
+            currentSpawnWait = GetRandomSpawnWait();
+        }
+    }
+
+    private float GetRandomSpawnWait()
+    {
+        return Random.Range(averageSpawnTime - spawnIntervalWidth, averageSpawnTime + spawnIntervalWidth);
+    }
+
+    private Vector3 GetRandomSpawnPosition()
+    {
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        Vector3 offset = new Vector3(spawnRadius * Mathf.Cos(angle), spawnHeight, spawnRadius * Mathf.Sin(angle));
+        return playerTransform.position + offset;
+    }
+
+    private void SpawnSwarm()
+    {
+        GameObject swarm = Instantiate(swarmPrefab);
+        swarm.GetComponent<Swarm>().SetPlayerTransform(playerTransform);
+        swarm.GetComponent<Swarm>().SetPosition(GetRandomSpawnPosition());
+        swarm.GetComponent<Swarm>().SetSpeed(speed);
+    }
+}
